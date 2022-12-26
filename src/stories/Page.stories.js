fe4f5e09f0c8963cd7480fb11f85152fb1f10e50
Mario Tavarez <mjtavarez@maxillc.com>
2022-12-26 17:15:31 -0600

    ci(linter): :wrench: eslint & prettier & husky configuration files

--- src/stories/Page.stories.js
+++ src/stories/Page.stories.js
@@ -2,11 +2,11 @@ import { within, userEvent } from '@storybook/testing-library';
 import { createPage } from './Page';
 
 export default {
-  title: 'Example/Page',
-  parameters: {
-    // More on Story layout: https://storybook.js.org/docs/html/configure/story-layout
-    layout: 'fullscreen',
-  },
+	title: 'Example/Page',
+	parameters: {
+		// More on Story layout: https://storybook.js.org/docs/html/configure/story-layout
+		layout: 'fullscreen',
+	},
 };
 
 const Template = () => createPage();
@@ -17,7 +17,7 @@ export const LoggedIn = Template.bind({});
 
 // More on interaction testing: https://storybook.js.org/docs/html/writing-tests/interaction-testing
 LoggedIn.play = async ({ canvasElement }) => {
-  const canvas = within(canvasElement);
-  const loginButton = await canvas.getByRole('button', { name: /Log in/i });
-  await userEvent.click(loginButton);
+	const canvas = within(canvasElement);
+	const loginButton = await canvas.getByRole('button', { name: /Log in/i });
+	await userEvent.click(loginButton);
 };
